e1fa9e121eab674a04a5af1abe2ca74b2f3ff93e
Bansi1007 <1bansivaghasiya@gmail.com>
2026-05-29 13:57:18 -0400

    add Day5, remove Day3

---
+++ src/week2/Day7.java
@@ -0,0 +1,14 @@
+package week2;
+
+import java.util.Scanner;
+
+public class Day2 {
+   public static void main(String[] args) {
+
+
+    }
+    
+    static void Q1(){
+        Scanner input = new Scanner(System.in);
+    }
+}
