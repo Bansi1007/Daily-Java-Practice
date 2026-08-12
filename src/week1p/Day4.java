bbcb7ef67cfa01d87ff62fcab1bfc4e9bdff6988
Bansi1007 <1bansivaghasiya@gmail.com>
2026-08-12 13:41:54 -0400

    stack & exception

--- src/week1p/Day4.java
+++ src/week1p/Day4.java
@@ -20,29 +20,25 @@ public class Day4 {
             Example. {[]} -> true ([)] -> false*/
 
     public static boolean bracketsCheck(String str) {
-       Stack<Character> stack = new Stack<>();
-       for(char c:str.toCharArray()){
-           if (c=='['||c=='{'||c=='('){
-               stack.push(c);
-           }
-           else{
-               if (stack.isEmpty()){
-                   System.out.println("false");
-                   return false;
-               }
-               char top = stack.pop();
-               if (c==']'&&top!='['||c=='}'&&top!='{'||c==')'&&top!='('){
-                   System.out.println("false");
-                   return false;
-               }
-           }
-       }
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '[' || c == '{' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    System.out.println("false");
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ']' && top != '[' || c == '}' && top != '{' || c == ')' && top != '(') {
+                    System.out.println("false");
+                    return false;
+                }
+            }
+        }
         System.out.println("true");
         return stack.isEmpty();
     }
-
-
-
 
 
 }
@@ -51,38 +47,40 @@ public class Day4 {
 
     Example. push 5, push 2, getMin -> 2, pop, getMin -> 5*/
 
-  class stackOperation {
-      Stack<Integer> stack = new Stack<>();
-      Stack<Integer> min = new Stack<>();
+class stackOperation {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> min = new Stack<>();
 
-      public void push(int x){
-          stack.push(x);
+    public void push(int x) {
+        stack.push(x);
 
-          if (min.isEmpty() ||x<min.peek()){
-              min.push(x);
-          }
-          else {
-              min.push(min.peek());
-          }
-      }
+        if (min.isEmpty() || x < min.peek()) {
+            min.push(x);
+        } else {
+            min.push(min.peek());
+        }
+    }
 
-      public void pop(){
-          if (stack.isEmpty()){
-              return;
-          }
-          stack.pop();
-          min.pop();
-      }
-      public int peek(){
-          if (stack.isEmpty()){
-              return -1;
-          }
-          return stack.peek();
-      }
-      public int getMin(){
-          if (stack.isEmpty()){
-              return -1;
-          }
-          return min.peek();
-      }
-  }
+    public void pop() {
+
+        if (stack.isEmpty()) {
+            return;
+        }
+        stack.pop();
+        min.pop();
+    }
+
+    public int peek() {
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        return stack.peek();
+    }
+
+    public int getMin() {
+        if (stack.isEmpty()) {
+            return -1;
+        }
+        return min.peek();
+    }
+}
